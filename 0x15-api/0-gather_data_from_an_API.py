d646fb1b6bbafbcae33c89763bec8ca487e47f3a
OSE <117799165+osenoah@users.noreply.github.com>
2023-07-25 02:32:15 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
-
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Fetching the employee's information
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data["name"]
-
-    # Fetching the employee's TODO list
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Counting completed tasks and total tasks
-    completed_tasks = [task for task in todos_data if task["completed"]]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todos_data)
-
-    # Printing the output in the required format
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: The employee ID must be an integer.")
-        sys.exit(1)
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
-    get_employee_todo_progress(employee_id)
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
 
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
