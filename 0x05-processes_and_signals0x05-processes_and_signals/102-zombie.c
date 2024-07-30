de32064425be44942434682c9fb6e81194a01f89
OSE <117799165+osenoah@users.noreply.github.com>
2024-07-30 20:39:08 +0100

    Create 5 Zombie Processes

---
+++ 0x05-processes_and_signals0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			dprintf(1, "Zombie process created, PID: %d\n", getpid());
+			return (0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
