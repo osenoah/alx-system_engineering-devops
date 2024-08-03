6afac8d475b5c8acba6a10e529575549eea2383d
OSE <117799165+osenoah@users.noreply.github.com>
2024-08-03 07:25:45 +0100

    API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
+
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
+   
     u_agent = 'Mozilla/5.0'
 
     headers = {
